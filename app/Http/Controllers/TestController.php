d1d8ea0f9ee0046556d97184dbee32f0a450f480
anderdavid <root@localhost.localdomain>
2021-04-18 18:04:13 -0500

    endpoint fake

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,10 +10,29 @@ class TestController extends Controller
 
     public function hello(Request $request){
 
-        $response["status"]=true;
-        $response["msg"]="prueba exitosa";
+        //$response["status"]=true;
+        //$response["msg"]="prueba exitosa";
 
-        return json_encode($response);
+        $data ='{
+                    "status": 200,
+                    "success": true,
+                    "message": "",
+                    "data": [
+                        {
+                          "id": 2076571,
+                          "scl2_id": "26001037",
+                          "location_scl2": 4,
+                          "puesto": "Pc loma colorada (bosconia - cesar)",
+                          "observation": "SABIGO-PRA",
+                          "date_reported": "2021-04-18 17:23:40",
+                          "dealing_id": 2
+                        }
+                    ]
+                }';
+
+        return $data;
+
+        //return json_encode($response);
     }
     /**
      * Display a listing of the resource.
